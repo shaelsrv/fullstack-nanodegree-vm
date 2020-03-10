7fc9abd6121462875462b65d7c7595d9451e846f
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-10 12:31:40 -0500

    uploading

--- random.sh
+++ random.sh
@@ -4,6 +4,8 @@
 for i in {1..50}
 do
 	echo "writing $i file"
-	head -c 1024KB /dev/random > "$i".gr
+	name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''
+	echo $name
+	head -c 1024KB /dev/random > $name.java
 done
 
