6946a2a5c6cb983f370887d30a19788cf22bd39d
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-10 12:33:33 -0500

    tttt

--- random.sh
+++ random.sh
@@ -6,6 +6,6 @@ do
 	echo "writing $i file"
 	name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''
 	echo $name
-	head -c 1024KB /dev/random > $name.java
+	head -c 1024KB /dev/random > "$i""$name".java
 done
 
