f654134216c52cd3cdf302e45737cdada73cca9e
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-10 12:55:06 -0500

    nj

--- random.sh
+++ random.sh
@@ -6,6 +6,6 @@ do
 	echo "writing $i file"
 	name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo ''
 	echo $name
-	head -c 1024KB /dev/random > "$i".py
+	head -c 1024KB /dev/random > "$i".c
 done
 
