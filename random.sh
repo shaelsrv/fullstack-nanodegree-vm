1d0d826e33f0f11072b0cf821e31cb6168818fa8
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-10 12:36:10 -0500

    rrrr

--- random.sh
+++ random.sh
@@ -4,8 +4,8 @@
 for i in {1..50}
 do
 	echo "writing $i file"
-	name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''
+	name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo ''
 	echo $name
-	head -c 1024KB /dev/random > "$i""$name".java
+	head -c 1024KB /dev/random > "$name_".ja
 done
 
