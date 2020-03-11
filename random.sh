0e3b43293fde680165dd94fd209979d05a3c1aae
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-11 11:28:47 -0500

    kkkk

--- random.sh
+++ random.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in {1..250}
+for i in {1..25}
 do
 	echo "writing $i file"
 	#name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo ''
 	#echo $name
-	head -c 1024KB /dev/random > "$i".c
+	head -c 10MB /dev/random > "$i".c
 done
 
