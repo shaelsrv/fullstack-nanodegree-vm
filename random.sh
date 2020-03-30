f7f0c5ccf8d16f2a98a5da01fbb45d307c253732
Sail Saurabh <shael.saurabh@gmail.com>
2020-03-30 11:48:17 -0500

    dfg

--- random.sh
+++ random.sh
@@ -5,6 +5,6 @@ do
 	#echo "writing $i file"
 	#name= head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo ''
 	#echo $name
-	head -c 900KB /dev/random > "$i".c
+	head -c 900KB /dev/random > "$i".javac
 done
 
